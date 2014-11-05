fbc6a9a6fca9067033599c9e4ab51be350889ea9
Dr. Bizarre <oscar@drbizarre.net>
2014-11-04 16:01:51 -0800

    integracion de nexmo y mailgun

--- custom.js
+++ custom.js
@@ -1,5 +1,47 @@
 
 // Update configuration to our liking
 $( document ).on( "mobileinit", function() {
- alert("asd");
+
+	$( "#envialo" ).click(function() {
+		$("#suceso").hide();
+	}); 
+
+});
+$( document ).ready(function() {
+$('#form1').validate({
+    rules: {
+        correo: {
+            required: true
+        },
+        suceso: {
+            required: true
+        }        
+    },
+    messages: {
+        correo: {
+            required: "Escribe tu correo."
+        },   	
+        suceso: {
+            required: "Cuentanos la urgencia."
+        }
+    },
+    errorPlacement: function (error, element) {
+        error.appendTo(element.parent().prev());
+    },
+    submitHandler: function (form) {
+    	$("#correo").val("");
+    	$("#suceso").val("");
+    	$.post( "http://whynot.mx/api/enviar", { correo: $("#correo").val(), mensaje: $("#suceso").val() })
+.done(function( data ) {
+
+});
+$.get( "https://rest.nexmo.com/sms/json", { api_key: "dedd250e", api_secret: "b879f1e6", from: "WHYNOT", to: "526641108064", text: "haz recibido una nueva peticion de ayuda desde la aplicacion de movil de whynot"  } );
+
+        $(':mobile-pagecontainer').pagecontainer('change', '#success', {
+            reload: false
+        });
+        return false;
+        
+    }
+});
 });
